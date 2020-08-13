1e473871cf0d1c129f6a8508e539e2e515bc819c
HugoMahatdejkul <69615454+HugoMahatdejkul@users.noreply.github.com>
2020-08-13 10:52:56 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Hugo Mahatdejkul");
+		System.out.println("One of my hobbies is building and messing around with keyboards");
+		System.out.println("I really like playing FPS games");
+		System.out.println("I don't really know what college I want to go to, but I think a UC is mostlikely");
+		System.out.println("I think I want to major in Bio, but I'm not 100% sure");
+	}
+}
